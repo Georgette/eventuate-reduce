4211ea513954038f107846347b23a03a5596effd
Georgette Pincin <georgette@pincin.com>
2015-08-28 21:39:28 -0400

    initial function of reduce eventuate

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
-module.exports = function () {
+module.exports = function mkReducedEventuate (eventuate, reduce, init) {
+    // create a new eventuate with the parent's setting
+    var reducedEventuate = eventuate.factory({ monitorConsumers: eventuate.hasConsumer !== undefined })
+
+    // expose to external sources
+    reducedEventuate.upstreamConsumer = reduceConsumer
+
+    // destroy upstream consumer
+    reducedEventuate.unsubscribe = function reducedEventuateUnsubscribe () {
+        eventuate.removeConsumer(reduceConsumer)
+    }
+
+    // create new eventuate upstream consumer
+    eventuate(reduceConsumer)
+
+    return reducedEventuate
+    var lastValue = init;
+    function reduceConsumer (data) {
+
+        if ( typeof lastValue === 'undefined') {
+            lastValue = data
+        } else {
+            lastValue = reduce( lastValue, data, init )
+        }
+
+        // act on original producers payload, and emit(produce) event
+        reducedEventuate.produce(lastValue)
+    }
 }
